9ab081aee548104773fcdbc2431225f30201ad87
hebaallah <57576774+Hebaallah61@users.noreply.github.com>
2023-01-16 21:47:29 +0200

    C#
    Hello World

---
+++ C#/Day51/day1/helloworld/hw.cs
@@ -0,0 +1,28 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    internal class hw
+    {
+    }
+}
+*/
+
+namespace hwns
+{
+    class hellow
+    {
+        //static main funcyion
+        //should be static to call it without instance from it(object from class) clr which call main if we call it with object the first thing will run it will be ctor 
+        //class should be in namespace
+        public static void Main()
+        {
+            //class consol 
+            System.Console.WriteLine("hello world");
+        }
+    }
+}
